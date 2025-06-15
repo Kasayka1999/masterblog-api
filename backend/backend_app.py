846c5bc76c52e915ec30a1a31144ee229d5edfe2
Giouxel Kasaika <uxell@86ac6454-540a-46e1-9174-03e671e7fd37.fritz.box>
2025-06-15 22:40:36 +0200

    added swagger documentation -> /api/docs/

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,8 +1,8 @@
-import json
-from crypt import methods
-
+from flask_swagger_ui import get_swaggerui_blueprint
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+
+
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
@@ -11,6 +11,18 @@ POSTS = [
     {"id": 1, "title": "First post", "content": "This is the first post."},
     {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
+
+SWAGGER_URL="/api/docs"  # (1) swagger endpoint e.g. HTTP://localhost:5002/api/docs
+API_URL="/static/masterblog.json" # (2) ensure you create this dir and file
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Masterblog API' # (3) You can change this if you like
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 def validate_post_data(new_data):
     if "title" not in new_data or "content" not in new_data:
